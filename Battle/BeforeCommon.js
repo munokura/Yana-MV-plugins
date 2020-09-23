043a74837f9b3db86596c50c5c011ddf4f45f6d0
munokura <munokura@users.noreply.github.com>
2020-09-23 19:11:14 +0900

    注意事項追記

--- Battle/BeforeCommon.js
+++ Battle/BeforeCommon.js
@@ -14,20 +14,32 @@ var Imported = Imported || {};
 Imported['BeforeCommon'] = 1.01;
 
 /*:
+ * @target MZ MV
+ * @url https://raw.githubusercontent.com/munokura/Yana-MV-plugins/master/Battle/BeforeCommon.js
  * @plugindesc ver1.01/スキルやアイテムの発動前に、スキルやアイテムに設定されたコモンイベントを発生させます。
  * @author Yana
  *
  * @param IndexVariableID
+ * @text 発動者インデックス変数ID
+ * @type variable
  * @desc 発動者のインデックスを保存する変数IDです。
  * 発動者がエネミーの場合は値に+1000されます。
  * @default 11
  *
  * @param TargetIndexVariableID
+ * @text 対象インデックス変数ID
+ * @type variable
  * @desc 対象のインデックスを保存する変数IDです。
  * 対象が2体以上の場合は変数の値は-1が設定されます。
  * @default 12
  *
- * @help------------------------------------------------------
+ * @help
+ * 注意事項（ムノクラ追記）
+ * ------------------------------------------------------
+ * RPGツクールMZでターン制での動作を確認。
+ * タイムプログレス制では動作しません。
+ * 
+ * ------------------------------------------------------
  *  プラグインコマンドはありません。
  * ------------------------------------------------------
  * ------------------------------------------------------
@@ -41,8 +53,10 @@ Imported['BeforeCommon'] = 1.01;
  * と記述することで、スキルやアイテムが発生する前に、
  * IDがx番のコモンイベントの実行を予約します。
  *
- * また、IndexVariableIDで指定した番号の変数に、行動者のインデックスが、
- * TargetIndexVariableIDで指定した番号の変数に、対象のインデックスが格納されます。
+ * また、IndexVariableID(発動者インデックス変数ID)で指定した番号の変数に、
+ * 行動者のインデックスが、
+ * TargetIndexVariableID(対象インデックス変数ID)で指定した番号の変数に、
+ * 対象のインデックスが格納されます。
  * ただし、対象のインデックスは対象が単体の時しか格納されず、かつ、
  * エネミーの行動では、コモンを実行するタイミングで対象が決定していないため、
  * 対象には-1(特定不可)が設定されます。
@@ -55,13 +69,13 @@ Imported['BeforeCommon'] = 1.01;
  * 二次配布も制限はしませんが、サポートは行いません。
  * 著作表示は任意です。行わなくても利用できます。
  * 要するに、特に規約はありません。
- * バグ報告や使用方法等のお問合せはネ実ツクールスレ、または、Twitterにお願いします。
- * https://twitter.com/yanatsuki_
+ * バグ報告や使用方法等のお問合せはネ実ツクールスレ、
+ * または、Twitterにお願いします。
+ * https://twitter.com/yanatsuki_/
  * 素材利用は自己責任でお願いします。
+ * 
  * ------------------------------------------------------
  * 更新履歴:
- * RPGツクールMZでの一部動作を確認。
- * 対象のインデックスを変数に代入する機能はMZでは機能していない。
  * ver1.01:
  * 対象を保存する変数の設定を追加。
  * ver1.00:
@@ -69,7 +83,7 @@ Imported['BeforeCommon'] = 1.01;
  */
 
 
-(function() {
+(function () {
 
     'use strict';
 
@@ -81,14 +95,14 @@ Imported['BeforeCommon'] = 1.01;
 
     ////////////////////////////////////////////////////////////////////////////////////
 
-    DataManager.isBeforeCommon = function(item) {
+    DataManager.isBeforeCommon = function (item) {
         if (!item) { return false }
         if (item.meta['発動前コモン']) { return true }
         if (item.meta['BeforeCommon']) { return true }
         return false;
     };
 
-    DataManager.beforeCommonEffect = function(item) {
+    DataManager.beforeCommonEffect = function (item) {
         var effects = [];
         if (item.meta['発動前コモン']) {
             effects = Number(item.meta['発動前コモン']);
@@ -101,14 +115,14 @@ Imported['BeforeCommon'] = 1.01;
     ////////////////////////////////////////////////////////////////////////////////////
 
     var __BManager_startAction = BattleManager.startAction;
-    BattleManager.startAction = function() {
+    BattleManager.startAction = function () {
         var action = this._subject.currentAction();
         if (this.checkBeforeCommon(action)) { return }
         __BManager_startAction.call(this);
         this._execBeforeCommon = false;
     };
 
-    BattleManager.checkBeforeCommon = function(action) {
+    BattleManager.checkBeforeCommon = function (action) {
         if (action && !this._execBeforeCommon && DataManager.isBeforeCommon(action.item())) {
             this._execBeforeCommon = true;
             var beforeCommon = DataManager.beforeCommonEffect(action.item());
@@ -130,7 +144,7 @@ Imported['BeforeCommon'] = 1.01;
     ////////////////////////////////////////////////////////////////////////////////////
 
     var __GBattler_removeCurrentAction = Game_Battler.prototype.removeCurrentAction;
-    Game_Battler.prototype.removeCurrentAction = function() {
+    Game_Battler.prototype.removeCurrentAction = function () {
         if (!BattleManager._execBeforeCommon) {
             __GBattler_removeCurrentAction.call(this);
         }
