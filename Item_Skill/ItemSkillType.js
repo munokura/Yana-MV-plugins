972b299ab5beef809c337fa8d51e5558d09630ae
munokura <36838778+munokura@users.noreply.github.com>
2020-10-20 13:21:45 +0900

    MZで動作確認

--- Item_Skill/ItemSkillType.js
+++ Item_Skill/ItemSkillType.js
@@ -13,14 +13,19 @@
 var Imported = Imported || {};
 Imported['ItemSkillType'] = 1.00;
 /*:
+ * @target MZ MV
+ * @url https://raw.githubusercontent.com/munokura/Yana-MV-plugins/master/Item_Skill/ItemSkillType.js
  * @plugindesc ver1.00/アイテムにスキルタイプを関連付けることで、アイテムコマンドの封印などを可能にします。
  * @author Yana
  * 
  * @param ItemSkillType
+ * @text アイテムのスキルタイプID
  * @desc アイテムのスキルタイプとして設定するIDです。
+ * @type number
  * @default 4
  * 
- * @help ------------------------------------------------------
+ * @help
+ * ------------------------------------------------------
  * 使用方法
  * ------------------------------------------------------
  * プラグインパラメータを設定すれば、動作します。
@@ -33,7 +38,8 @@ Imported['ItemSkillType'] = 1.00;
  * 二次配布も制限はしませんが、サポートは行いません。
  * 著作表示は任意です。行わなくても利用できます。
  * 要するに、特に規約はありません。
- * バグ報告や使用方法等のお問合せはネ実ツクールスレ、または、Twitterにお願いします。
+ * バグ報告や使用方法等のお問合せはネ実ツクールスレ、
+ * または、Twitterにお願いします。
  * https://twitter.com/yanatsuki_
  * 素材利用は自己責任でお願いします。
  * ------------------------------------------------------
@@ -41,18 +47,18 @@ Imported['ItemSkillType'] = 1.00;
  * ver1.00:
  * 公開
  */
-(function(){
-	////////////////////////////////////////////////////////////////////////////////////
-	
-	var parameters = PluginManager.parameters('ItemSkillType');
-	var itemSkillType = Number(parameters['ItemSkillType']);
-	
-	////////////////////////////////////////////////////////////////////////////////////
-	
-	// 再定義
-	Window_ActorCommand.prototype.addItemCommand = function() {
-    	this.addCommand(TextManager.item, 'item', !this._actor.isSkillTypeSealed(itemSkillType));
-	};
-	
-	////////////////////////////////////////////////////////////////////////////////////
+(function() {
+    ////////////////////////////////////////////////////////////////////////////////////
+
+    var parameters = PluginManager.parameters('ItemSkillType');
+    var itemSkillType = Number(parameters['ItemSkillType']);
+
+    ////////////////////////////////////////////////////////////////////////////////////
+
+    // 再定義
+    Window_ActorCommand.prototype.addItemCommand = function() {
+        this.addCommand(TextManager.item, 'item', !this._actor.isSkillTypeSealed(itemSkillType));
+    };
+
+    ////////////////////////////////////////////////////////////////////////////////////
 }());
